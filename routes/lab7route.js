0edf0d7547bcd39d99c16b33e6167bb95abd9a4a
EC2 Default User <ec2-user@ip-172-31-24-28.us-east-2.compute.internal>
2019-11-05 22:47:23 +0000

    11-05-19

--- routes/lab7route.js
+++ routes/lab7route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/lab7', function(req, res, next) {
-  res.render('/labs/lab7/views/index');
+  res.render('/labs/lab7/app.js');
 });
 
 module.exports = router;
